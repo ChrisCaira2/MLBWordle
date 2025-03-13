5a0d720a4605f82953f408afc8601a20c4c6b7bf
Chris <ccaira1401@gmail.com>
2025-03-13 11:32:26 -0500

    Serve React frontend from Express backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('/api', (req, res) => {
     res.send('Hello World!');
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
 
 app.listen(port, () => {
